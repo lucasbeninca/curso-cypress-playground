c0956243872746abc0d41a81a2be7d5461dd4e84
Lucas Beninca <lucasbeninca@proton.me>
2025-06-06 21:14:45 -0300

    erro aula 03 uso do contains

--- cypress/e2e/playgrond.cy.js
+++ cypress/e2e/playgrond.cy.js
@@ -3,7 +3,20 @@ describe('template spec', () => {
     cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html');
   });  
 
-  it('acessando aplicação cypress playground', () => {
+   it('shows promotional banner', () => {
+     cy.get('#promotional-banner').should('be.visible')
+  });
+
+  it('verify title', () => {
      cy.get('#title').should('have.text', '🌲 Cypress Playground 🛝');
   });
+
+  it.only('clicks the Subscribe button and shows a succes message', () => {
+     cy.contains('button','Subscribe').click()
+   
+     cy.contains('#success',
+      "'You've been successfully subscribed to our newsletter."
+     ).should('be.visible')
+    })
+
 });
