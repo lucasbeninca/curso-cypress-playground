91f73de1af03c0efdf3c2f3864b11508b814a3d8
Lucas Beninca <lucasbeninca@proton.me>
2025-06-10 11:49:00 -0300

    lesson 18

--- cypress/e2e/playgrond.cy.js
+++ cypress/e2e/playgrond.cy.js
@@ -11,12 +11,36 @@ describe('template spec', () => {
      cy.get('#title').should('have.text', '🌲 Cypress Playground 🛝');
   });
 
-  it.only('clicks the Subscribe button and shows a succes message', () => {
+  it('clicks the Subscribe button and shows a succes message', () => {
      cy.contains('button','Subscribe').click()
+     cy.get('#success')
+    .should('have.text', "You've been successfully subscribed to our newsletter.")
+    .should('be.visible');
+  })
+
+  it('type in a input which', () => {
+     cy.get('#signature-textarea').type('Cypress is awesome!')
+     cy.get('#signature').should('have.text', 'Cypress is awesome!')
+  })
+
+  it('type in a second input text and check a uncheck', () =>{
+     cy.get('#signature-textarea-with-checkbox').type('Lucas')
+     cy.get('#signature-checkbox').check()
+     cy.get('#signature-triggered-by-check').should('have.text','Lucas')
+     cy.get('#signature-checkbox').uncheck()
+     cy.get('#signature-triggered-by-check').should('not.have.text','Lucas')
+  })
    
-     cy.contains('#success',
-      "'You've been successfully subscribed to our newsletter."
-     ).should('be.visible')
-    })
+  it.only('check and uncheck radio button', () =>{
+    cy.get('#off').check()
+    cy.get('#on-off').should('have.text','OFF')
+    cy.get('#on').check()
+    cy.get('#on-off').should('have.text','ON')
+
+
+
+  })
+
+
 
 });
